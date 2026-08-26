aaf6af50156ec8e7f79d085d6a518272fbcc6690
Nazmul Islam <hello.nazzmul@gmail.com>
2026-08-27 01:54:52 +0600

    feat: update corporate address to Uttara Sector 07 and phone 01907075173

--- js/app.js
+++ js/app.js
@@ -106,9 +106,9 @@ const PRODUCTS_DATA = [
 const SHOWROOMS_DATA = {
     dhaka: [
         {
-            name: "Heeds Ceramics Corporate HQ & Experience Center",
-            address: "Lift#4, House # 13, Road-17/A, Sector-12, Uttara, Dhaka-1230",
-            phone: "+8809612677677",
+            name: "Heeds Ceramics Industries Ltd - Corporate HQ",
+            address: "House no: 03, Road no: 35, Sector 07, Uttara, Dhaka 1230",
+            phone: "+8801907075173",
             hours: "9:00 AM - 8:00 PM (Sat - Thu)",
             mapsUrl: "https://maps.google.com"
         }
